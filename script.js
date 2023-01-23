f9d8ae3cf12539a2098c565df1797737c80277cf
thesneilert <eilert.gaming@outlook.com>
2023-01-23 12:58:00 +0100

    functions fixed

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const valgfrittText = document.getElementById("valgfrittText")
+const output = document.getElementById("storyOutput");
+let valgfrittOrd1 = "____";
+let valgfrittOrd2 = "____";
+let valgfrittOrd3 = "____";
+let valgfrittOrd4 = "____";
+let valgfrittOrd5 = "____";
+buildStory();   
 
-var valgfrittArray = ["_______","_______","_______","_______","_______",]
-
-const valgfrittMuligheter = [
-    [dum, god, umulig],
-    [kort, lang, ubrukelig],
-    [kort, lang, ubrukelig],
-    [kjipe, morsomme, middelmådige],
-    [meningsløse, håpløse, adjektive]
-]
-
-function valgfrittFunksjonNavn(){
-    valgfrittText.innerHTML = `<p>En dag hadde Eilert en ${valgfrittArray[0]} idè, han skulle lage en ${valgfrittArray[1]} adjektivhistorie.
-    Kanskje ble det ikke fullt en så ${valgfrittArray[2]} historie, men du fikk hvertfall satt inn dine egne ${valgfrittArray[3]} ord.
-    Faktisk var det ikke dine helt egne ord heller, for Eilert ga deg kun tre ${valgfrittArray[4]} valgmuligheter.<p>`
+function buildStory(){
+    output.innerHTML = `
+    En dag hadde Eilert en ${valgfrittOrd1} idè, han skulle lage en ${valgfrittOrd2} adjektivhistorie.<br>
+    Kanskje ble det ikke fullt en så ${valgfrittOrd3} historie, men du fikk hvertfall satt inn dine egne ${valgfrittOrd4} ord.<br>
+    Faktisk var det ikke dine helt egne ord heller, for Eilert ga deg kun tre ${valgfrittOrd5} valgmuligheter.
+    `;
 }
 
-function valgfrittByttText(ord){
+function valgfrittOrdClick1(choice){
+    valgfrittOrd1 = choice.innerHTML;
+    buildStory(); 
+}    
 
+function valgfrittOrdClick2(choice){
+    valgfrittOrd2 = choice.innerHTML;
+    buildStory(); 
+}
+
+function valgfrittOrdClick3(choice){
+    valgfrittOrd3 = choice.innerHTML;
+    buildStory(); 
+}
+
+function valgfrittOrdClick4(choice){
+    valgfrittOrd4 = choice.innerHTML;
+    buildStory(); 
+}
+function valgfrittOrdClick5(choice){
+    valgfrittOrd5 = choice.innerHTML;
+    buildStory(); 
 }
